570e5201648b4332b303ee21226a8627a621ef31
beijing <logey1314@gmail.com>
2026-07-07 17:39:26 +0800

    第5题

--- 三数之和/双指针.py
+++ 三数之和/双指针.py
@@ -22,6 +22,10 @@ class Solution:
                 else:
                     # 和为0，记录下标
                     result.append([nums[i], nums[j], nums[k]])
+                    while j < k and nums[j] == nums[j + 1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k - 1]:
+                        k -= 1
                     j += 1
                     k -= 1
 
